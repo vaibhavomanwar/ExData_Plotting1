eff022fe6f7104385ed3ffc97f89b9ff2c79dec5
Vaibhav Omanwar <vaibhavomanwar@gmail.com>
2014-07-12 21:57:52 +0900

    Exploratory data analysis, course project 1 - Analysis of household energy variation based on dataset from UC Irvine ML Repo.

---
+++ plot2.R
@@ -0,0 +1,31 @@
+## Introduction
+# This file uses data from UC Irvine Machine Learning Repository in particular it uses
+# "Individual household electric power consumption Data Set".
+# Overall goal here is simply to examine how household energy usage varies over a 2-day period in February, 2007.
+
+# First download the dataset from UC Irvine ML Repo url.
+# The dataset is quite big, it may take some time to download the entire dataset.
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(url, dst = "household_power_consumption.zip")
+# uncompress the downloaded dataset 
+unzip("household_power_consumption.zip")
+
+# read few rows to get classes
+fewrows <- read.table("household_power_consumption.txt", header = T, sep = ";", nrows = 5)
+classes <- sapply(fewrows, class)
+
+# read entire data with know classes
+# reading may few mins
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", colClasses = classes)
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# obtain the subset of entire dataset for specific dates
+fromdate <- as.Date("2007-02-01")
+todate <- as.Date("2007-02-02")
+subdata <- subset(data, Date >= fromdate & Date <= todate)
+dateTime <- strptime(paste(subdata$Date, sabdata$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# plot histogram of Global Active Power
+png(file = "plot2.png")
+plot(dateTime, subdata$Global_active_power, type = 'l', ylab = "Global Active Power(killowatts)", xlab = "")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,34 @@
+## Introduction
+# This file uses data from UC Irvine Machine Learning Repository in particular it uses
+# "Individual household electric power consumption Data Set".
+# Overall goal here is simply to examine how household energy usage varies over a 2-day period in February, 2007.
+
+# First download the dataset from UC Irvine ML Repo url.
+# The dataset is quite big, it may take some time to download the entire dataset.
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(url, dst = "household_power_consumption.zip")
+# uncompress the downloaded dataset 
+unzip("household_power_consumption.zip")
+
+# read few rows to get classes
+fewrows <- read.table("household_power_consumption.txt", header = T, sep = ";", nrows = 5)
+classes <- sapply(fewrows, class)
+
+# read entire data with know classes
+# reading may few mins
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", colClasses = classes)
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# obtain the subset of entire dataset for specific dates
+fromdate <- as.Date("2007-02-01")
+todate <- as.Date("2007-02-02")
+subdata <- subset(data, Date >= fromdate & Date <= todate)
+dateTime <- strptime(paste(subdata$Date, sabdata$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# plot histogram of Global Active Power
+png(file = "plot3.png")
+plot (dateTime,subdata$Sub_metering_1,type = "l", xlab = "", ylab = "Energy sub metering", col = "black")
+lines (dateTime,subdata$Sub_metering_2,type = "l", col = "red")
+points (dateTime,subdata$Sub_metering_3,type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.off()
